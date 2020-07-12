4d589b73e8d7f4c35cbf8239f8d483025396eab4
Enaria Vishenda <matthewfarrel@yandex.ru>
2020-07-12 19:38:39 +0500

    Create swapi-service and react-components files

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className="header d-flex">
+        </div>
+    )
+}
+
+export default Header
