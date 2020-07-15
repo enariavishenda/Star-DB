35c5f1fa3450b5872d95c69deeff90253caae5ee
Enaria Vishenda <matthewfarrel@yandex.ru>
2020-07-15 23:57:03 +0500

    Add new Component - css spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SwapiService from "../../services/swapi-sevice.js"
+import Loader from "../loader";
 
 import './random-planet.css'
 
@@ -7,7 +8,8 @@ export default class RandomPlanet extends Component {
 
 
     state = {
-        planet: {}
+        planet: {},
+        loading: true
     };
 
     swapi = new SwapiService();
@@ -18,8 +20,9 @@ export default class RandomPlanet extends Component {
     }
 
     onPlanetLoaded = (planet) => { //будем передавать эту функцию в другую функцию
-        this.setState({ planet }) //вставляем весь уже трансформированный объект целиком из swapi, вместо того, чтобы вытаскивать по каждому свойству.
+        this.setState({ planet, loading: false }) //вставляем весь уже трансформированный объект целиком из swapi, вместо того, чтобы вытаскивать по каждому свойству.
         //а так же нам нужен уже объект вместе с id для изображения, id реализован в swapi регулярным выражением url'a
+        //если у нас есть планета, то Loader не возвращается
     }
 
     updatePlanet () {
@@ -31,9 +34,11 @@ export default class RandomPlanet extends Component {
 
     render() {
 
-        const { planet: {id, name, population, rotation, diameter }} = this.state
+        const { planet, loading } = this.state
 
-        console.log(id)
+        const load = loading ? <Loader /> : null
+        const content = !loading ? <JSX_Planet planet={planet}/> : null
+
 
         return (
 
@@ -42,27 +47,39 @@ export default class RandomPlanet extends Component {
                     Random Place
                 </h5>
                 <div className="random-planet jumbotron rounded">
-                    <img className="planet-image"
-                         src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-                    <div>
-                        <h4>{name}</h4>
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item">
-                                <span className="term">Population</span>
-                                <span>{population}</span>
-                            </li>
-                            <li className="list-group-item">
-                                <span className="term">Rotation Period</span>
-                                <span>{rotation}</span>
-                            </li>
-                            <li className="list-group-item">
-                                <span className="term">Diameter</span>
-                                <span>{diameter}</span>
-                            </li>
-                        </ul>
-                    </div>
+                    {load}
+                    {content}
                 </div>
             </div>
         );
     }
+}
+
+const JSX_Planet = ({ planet }) => {
+
+    const { id, name, population, rotation, diameter } = planet
+
+    return ( // для более двух DOM-элементов, клиент обертка для группировки элементов
+        <React.Fragment>
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+            <div>
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotation}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    )
 }
